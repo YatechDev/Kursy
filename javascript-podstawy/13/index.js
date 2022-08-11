b8b34d48906451324b5e6007003307eb5de1580a
YatechDev <106195439+YatechDev@users.noreply.github.com>
2022-08-11 20:22:57 +0100

    Lessons

---
+++ javascript-podstawy/13/index.js
@@ -0,0 +1,8 @@
+const numbers = [1, 3, 4, 5];
+
+console.log(numbers[0]);
+console.log(numbers.length);
+
+const arrays = [[1, 2, 4], ["Jacek"], [5, 6, 7]];
+
+console.log(arrays[0]);
---
+++ javascript-podstawy/15/index.js
@@ -0,0 +1,17 @@
+const arr = ["Tomek", "Kasia", "Jacek", "Mariusz"];
+
+// console.log(arr.length);
+//
+// arr.length = 2;
+// delete arr[2];
+//
+// arr. spice (2, 1);
+
+const last = arr.pop();
+const first = arr.shift();
+
+console.log(first);
+console.log(last);
+
+console.log(arr);
+
